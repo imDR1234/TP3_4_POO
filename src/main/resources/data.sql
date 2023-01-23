e0d042336233125986a9d8e54f0ef24e2feac484
Imane DRAOUI <im-dr@hotmail.fr>
2023-01-23 14:17:01 +0100

    updates

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO address (id, author, creation, content)VALUES (1, 'Imane', CURRENT_TIMESTAMP(), '16 Bd Charles nicolle ');
+INSERT INTO address (id, author, creation, content)VALUES (2, 'Draoui', CURRENT_TIMESTAMP(), 'Le mans france ');
+
